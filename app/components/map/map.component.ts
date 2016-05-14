f33b4dffa0367ca4ec42167a7dc58f00f92764a6
Agustin Burgos <agusburgos1602@gmail.com>
2016-05-14 17:16:19 -0300

    Instanced map and center to Argentina

--- app/components/map/map.component.ts
+++ app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'map-app',
@@ -6,6 +6,45 @@ import {Component} from '@angular/core';
   styleUrls: ['app/components/map/map.component.css']
 })
 
-export class MapComponent {
+export class MapComponent implements OnInit {
+  
+  /**
+   * Center map. Required.
+   */
+  center: google.maps.LatLng;
+
+  /**
+   * The initial map zoom level. Required.
+   */
+  zoom: number;
+  
+  /**
+   * The initial map mapTypeId. Defaults to ROADMAP.
+   */
+  mapTypeId: google.maps.MapTypeId;
+  
+  protected map: google.maps.Map;
+  
+  constructor(public elementRef: ElementRef) {
+    // Sets initial center map.
+    this.center = new google.maps.LatLng(-38.416097, -63.616672);
+
+    this.mapTypeId = google.maps.MapTypeId.ROADMAP;
+
+    // Sets the initial zoom.
+    this.zoom = 4;    
+  }
+  
+  ngOnInit() {
+    let mapOptions = {
+      center: this.center,
+      mapTypeId: this.mapTypeId,
+      zoom: <number>this.zoom      
+    };
+
+    // Instances the map.
+    var el: HTMLElement = this.elementRef.nativeElement.querySelector('#map');
+    this.map = new google.maps.Map(el, mapOptions);
+  }
 
 }
