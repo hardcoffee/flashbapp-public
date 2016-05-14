fa5e403365db0886189f628551f356932fd255e8
Lucio Martinez <martinezlucio.com@gmail.com>
2016-05-14 16:12:27 -0300

    Add angular 2 material npm packages

--- systemjs.config.js
+++ systemjs.config.js
@@ -9,7 +9,8 @@
     'app': 'app',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     'rxjs': 'node_modules/rxjs',
-    '@angular': 'node_modules/@angular'
+    '@angular': 'node_modules/@angular',
+    '@angular2-material': 'node_modules/@angular2-material'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
@@ -26,14 +27,31 @@
     '@angular/http',
     '@angular/platform-browser',
     '@angular/platform-browser-dynamic',
-    '@angular/router',
-    '@angular/router-deprecated',
-    '@angular/upgrade'
   ];
 
   // add package entries for angular packages in the form '@angular/http': { main: 'index.js', defaultExtension: 'js' }
   packageNames.forEach(function(pkgName) {
     packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  });
+
+  var materialPackageNames = [
+    "@angular2-material/core",
+    "@angular2-material/toolbar",
+    "@angular2-material/icon",
+    "@angular2-material/button",
+    "@angular2-material/sidenav",
+    "@angular2-material/list",
+    "@angular2-material/card",
+    "@angular2-material/input",
+    "@angular2-material/radio",
+    "@angular2-material/checkbox"
+  ];
+
+  // add package entries for matarial packages in the form
+  // '@angular2-material/input': { main: 'input.js', defaultExtension: 'js', format: 'cjs' }
+  materialPackageNames.forEach(function(pkgName) {
+    var startName = pkgName.indexOf('/') + 1;
+    packages[pkgName] = { main: pkgName.substr(startName)+'.js', defaultExtension: 'js', format: 'cjs' };
   });
 
   var config = {
