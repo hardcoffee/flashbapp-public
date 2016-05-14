23acda74584c4ad162cb79f2fcf39d8371a8b23f
Agustin Burgos <agusburgos1602@gmail.com>
2016-05-14 16:34:09 -0300

    Map component created

---
+++ app/components/map/map.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'map-app',
+  templateUrl: 'app/components/map/map.component.html',
+  styleUrls: ['app/components/map/map.component.css']
+})
+
+export class MapComponent {
+  
+}
