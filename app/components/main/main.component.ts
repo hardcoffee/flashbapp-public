51bb6d524913430f0321c8464af0702cd841312b
Agustin Burgos <agusburgos1602@gmail.com>
2016-05-14 16:45:02 -0300

    Display map component inside the map component

--- app/components/main/main.component.ts
+++ app/components/main/main.component.ts
@@ -1,7 +1,12 @@
 import {Component} from '@angular/core';
 
+import {MapComponent} from './../map/map.component';
+
 @Component({
   selector: 'my-app',
-  template: '<h1>AngularAttack 2016</h1><p>Our project runs!</p>'
+  template: '<h1>AngularAttack 2016</h1><p>Our project runs!</p><map-app></map-app>',
+  directives: [
+    MapComponent,
+  ]
 })
 export class MainComponent { }
